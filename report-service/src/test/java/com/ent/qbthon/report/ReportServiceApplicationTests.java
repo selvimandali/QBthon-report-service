ad089adf5c34f2fe8e8e5a4a14cf1a8b564f194e
ctsjavauser176 <ctsjavauser176@JavaCTS12.iiht.tech>
2020-05-25 19:01:22 +0530

    initial commit

---
+++ report-service/src/test/java/com/ent/qbthon/report/ReportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ent.qbthon.report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
